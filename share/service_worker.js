b5cb97bab3700019ad9a3439e4d77db646a8264c
KirkSuD <kirksud@gmail.com>
2025-01-27 00:13:04 +0800

    Improve account.html

--- share/service_worker.js
+++ share/service_worker.js
@@ -1,5 +1,5 @@
 // must update cache name on any web page change
-const cacheName = "kirksud_sw_cache_20250124_1900"
+const cacheName = "kirksud_sw_cache_20250126_1700"
 
 self.addEventListener("fetch", event => {
     event.respondWith((async () => {
