ff1b2484b8e66bf4544cfa898bd82c560c416545
KirkSuD <kirksud@gmail.com>
2025-02-08 09:18:30 +0800

    Optimize ktv.html: precompute singer song count

--- share/service_worker.js
+++ share/service_worker.js
@@ -1,5 +1,5 @@
 // must update cache name on any web page change
-const cacheName = "kirksud_sw_cache_20250208_0630"
+const cacheName = "kirksud_sw_cache_20250208_0900"
 
 self.addEventListener("fetch", event => {
     event.respondWith((async () => {
