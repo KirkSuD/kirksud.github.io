cf80857efc7fae6bc7021786996908d8361285d0
KirkSuD <kirksud@gmail.com>
2025-02-10 08:42:18 +0800

    Improve ktv.html: wiki singers, new search UI, optimizations

--- share/service_worker.js
+++ share/service_worker.js
@@ -1,5 +1,5 @@
 // must update cache name on any web page change
-const cacheName = "kirksud_sw_cache_20250208_0900"
+const cacheName = "kirksud_sw_cache_20250210_0800"
 
 self.addEventListener("fetch", event => {
     event.respondWith((async () => {
