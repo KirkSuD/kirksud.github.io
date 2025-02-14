33d69b55214d05d851533b0dab1bf10311480f14
KirkSuD <kirksud@gmail.com>
2025-02-15 00:52:46 +0800

    New feature: ktv.html: music player

--- share/service_worker.js
+++ share/service_worker.js
@@ -1,5 +1,5 @@
 // must update cache name on any web page change
-const cacheName = "kirksud_sw_cache_20250214_0000"
+const cacheName = "kirksud_sw_cache_20250215_0030"
 
 self.addEventListener("fetch", event => {
     event.respondWith((async () => {
